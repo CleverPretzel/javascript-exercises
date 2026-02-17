55d0fcd6aad2dbe66d0338d1801394725c513cea
Giedre <giedre.klovaite@gmail.com>
2026-02-17 14:55:22 -0600

    Complete Foundations Exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let expression = (fahrenheit - 32) * 5/9;
+  return Number.isInteger(expression) ? expression : Number(expression.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let expression = celsius * 9/5 + 32;
+  return Number.isInteger(expression) ? expression : Number(expression.toFixed(1));
 };
 
 // Do not edit below this line
