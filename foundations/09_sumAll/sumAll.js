167f5147fa30e065a2826dfad5099134208c2878
Giedre <giedre.klovaite@gmail.com>
2026-02-17 14:18:30 -0600

    Complete Foundations Exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  if (
+    num1 < 0 || 
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return 'ERROR';
+  }
+
+  let lowNum = (num1 < num2) ? num1 : num2;
+  let highNum = (num1 > num2) ? num1 : num2;
+
+  let sum = 0;
+
+  for (let i = lowNum; i <= highNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
