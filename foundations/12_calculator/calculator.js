f14ef5bd9517834d0e1c89e767626d2601738882
Giedre <giedre.klovaite@gmail.com>
2026-02-23 21:03:10 -0600

    Complete Foundations Exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, curr) => prev * curr);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+	let factorial = num;
+  for (i = 1; i < num; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
