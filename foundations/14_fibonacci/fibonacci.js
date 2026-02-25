a89879586ec18b784721a06c0c0e8411483ba846
Giedre <giedre.klovaite@gmail.com>
2026-02-24 20:15:04 -0600

    Complete Foundations Exercise 14
    My solutions tend to be different from the example solutions,
    which shows that I have a different way of thinking. Is that good? Is it bad?
    I don't know.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  if (position == 0) return 0;
+  if (position < 0) return 'OOPS';
 
+  let fibonacciSeq = [1, 1];
+  for (let i = 1; fibonacciSeq.length < position; i++) {
+    let previousEL = fibonacciSeq[i];
+    let previousPreviousEl = fibonacciSeq[i - 1];
+    fibonacciSeq.push(previousEL + previousPreviousEl);
+  }
+
+  return fibonacciSeq[position - 1];
 };
 
 // Do not edit below this line
