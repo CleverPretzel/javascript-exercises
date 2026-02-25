1dcf683d503f81176f24b4be1e76d101ec90086c
Giedre <giedre.klovaite@gmail.com>
2026-02-24 21:08:05 -0600

    Complete Foundations Exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrOfObj) {
 
+  arrOfObj.sort((a, b) => {
+    return ((b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth)
+    - ((a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth);
+  });
+
+  return arrOfObj[0];
 };
 
 // Do not edit below this line
